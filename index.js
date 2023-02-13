75daa77edd240cbea6cf2e920ab9db5890e918f0
gauravgulia26 <grvgulia007@gmail.com>
2023-02-14 01:50:09 +0530

    added the openweather api to track weather

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const parser = require("body-parser");
 const mongoose = require("mongoose");
-const { name } = require("ejs");
+const name = require("ejs");
+const axios = require("axios");
+// const ObjectID = require("mongodb").ObjectID;
 mongoose.set("strictQuery", true);
 
 const app = express();
@@ -21,6 +23,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27018/todo");
+  // await mongoose.connect("mongodb://127.0.0.1:27018/weather");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
@@ -34,9 +37,15 @@ const todo_schema = new mongoose.Schema({
   name: String,
 });
 
+const weather_schema = new mongoose.Schema({
+  name: String,
+  temperature: Number,
+});
+
 //*  creating model or database
 
 const todo = mongoose.model("todo", todo_schema);
+const weather = mongoose.model("weather", weather_schema);
 
 //* creating document inside the collection
 
@@ -52,17 +61,52 @@ const t3 = new todo({
 
 const defaults = [t1, t2, t3];
 
+// creating documents inside the weather collections
+
 //* Get request
 
 var today = new Date();
 
+//* making the api call openweather
+
+axios
+  .get(
+    "https://api.openweathermap.org/data/2.5/weather?lat=30.748882&lon=76.641357&appid=4850b25330e03671839805f08fc6e6c0&units=metric"
+  )
+  .then((response) => {
+    weather.count(function (err, count) {
+      if (!err && count === 0) {
+        const weather_data = new weather({
+          name: response.data.name,
+          temperature: response.data.main.temp,
+        });
+        weather_data.save();
+        console.log("insertion Successfull ");
+      }
+
+      //!updateone method is not working
+
+      weather.updateOne({ name: "Kharar" }, { $set: { temperature: 102 } });
+
+      console.log("everything is successfull !! ");
+      // console.log(weather.weather_data);
+      // console.log(response);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.get("/", (req, res) => {
+  // time data to render
+
   var options = {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
+
   var day = today.toLocaleDateString("en-US", options);
 
   todo.find({}, function (err, foundItems) {
@@ -76,12 +120,15 @@ app.get("/", (req, res) => {
       });
       res.redirect("/");
     } else {
-      res.render("app", { weekday: day, items: foundItems });
+      res.render("app", {
+        weekday: day,
+        items: foundItems,
+        city_name: "Kharar",
+        temperature: 100,
+      });
     }
-    // console.log(foundItems);
   });
 });
-// });
 
 //* post request
 
@@ -113,5 +160,5 @@ app.post("/success", (req, res) => {
 // deletion route
 
 app.post("/delete", (req, res) => {
-  console.log(req);
+  console.log(req.body);
 });
