9c6b88ebe8884f75447e801c58c77f58480d8a5b
gauravgulia26 <grvgulia007@gmail.com>
2023-02-15 01:31:23 +0530

    added mongodb cloud database

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ const parser = require("body-parser");
 const mongoose = require("mongoose");
 const name = require("ejs");
 const axios = require("axios");
-// const ObjectID = require("mongodb").ObjectID;
+const { getCurrentPosition } = require("geolocation");
+const favicon = require("serve-favicon");
+const path = require("path");
+const navigator = require("navigator");
+const { request } = require("http");
+const app = express();
 mongoose.set("strictQuery", true);
 
-const app = express();
 // var choices = ["Brush Teeth", "Drink Water", "Skincare"];
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+// app.use(favicon(path.join(__dirname, "public", "images/favicon.ico")));
 
 app.listen(3000, function () {
   console.log("hey i am working on port 3000");
 });
+
+// navigator.geolocation.getCurrentPosition((position) => {
+//   doSomething(position.coords.latitude, position.coords.longitude);
+// });
 
 app.set("view engine", "ejs");
 
@@ -22,8 +31,9 @@ app.set("view engine", "ejs");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27018/todo");
-  // await mongoose.connect("mongodb://127.0.0.1:27018/weather");
+  await mongoose.connect(
+    "mongodb+srv://gourav:helloworld123@cluster0.koe22yf.mongodb.net/todo"
+  );
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
@@ -45,7 +55,7 @@ const weather_schema = new mongoose.Schema({
 //*  creating model or database
 
 const todo = mongoose.model("todo", todo_schema);
-const weather = mongoose.model("weather", weather_schema);
+// const weather = mongoose.model("weather", weather_schema);
 
 //* creating document inside the collection
 
@@ -69,37 +79,11 @@ var today = new Date();
 
 //* making the api call openweather
 
-axios
-  .get(
-    "https://api.openweathermap.org/data/2.5/weather?lat=30.748882&lon=76.641357&appid=4850b25330e03671839805f08fc6e6c0&units=metric"
-  )
-  .then((response) => {
-    weather.count(function (err, count) {
-      if (!err && count === 0) {
-        const weather_data = new weather({
-          name: response.data.name,
-          temperature: response.data.main.temp,
-        });
-        weather_data.save();
-        console.log("insertion Successfull ");
-      }
+// axios;
+// axios
 
-      //!updateone method is not working
-
-      weather.updateOne({ name: "Kharar" }, { $set: { temperature: 102 } });
-
-      console.log("everything is successfull !! ");
-      // console.log(weather.weather_data);
-      // console.log(response);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
+// console.log(geolocation.getCurrentPosition());
 app.get("/", (req, res) => {
-  // time data to render
-
   var options = {
     weekday: "long",
     year: "numeric",
@@ -120,18 +104,31 @@ app.get("/", (req, res) => {
       });
       res.redirect("/");
     } else {
-      res.render("app", {
-        weekday: day,
-        items: foundItems,
-        city_name: "Kharar",
-        temperature: 100,
-      });
+      axios
+        .get(
+          "https://api.openweathermap.org/data/2.5/weather?lat=30.748882&lon=76.641357&appid=4850b25330e03671839805f08fc6e6c0&units=metric"
+        )
+        .then((response) => {
+          const wthr = [];
+          wthr.push(response.data.main.temp);
+          wthr.push(response.data.name);
+          wthr.push(response.data.cod);
+          res.render("app", {
+            weekday: day,
+            items: foundItems,
+            city_name: wthr[0],
+            temperature: wthr[1],
+            status_code: wthr[2],
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   });
 });
 
 //* post request
-
 app.post("/", (req, res) => {
   var choice = req.body.pending;
 
@@ -142,11 +139,9 @@ app.post("/", (req, res) => {
   });
 
   t4.save();
+  // making the redirect request
 
-  // array.forEach((element) => {});
-  // choices.push(choice);
-  res.redirect("/");
-  // console.log(req);
+  res.redirect("back");
 });
 
 app.post("/about", (req, res) => {
@@ -156,9 +151,10 @@ app.post("/about", (req, res) => {
 app.post("/success", (req, res) => {
   res.render("success");
 });
-
 // deletion route
 
 app.post("/delete", (req, res) => {
   console.log(req.body);
 });
+
+// console.log(getCurrentPosition());
